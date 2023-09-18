e0778d183d0e2921d4a410f81a40816217b819e9
BK Mahapatra <39907792+bkmahapatra@users.noreply.github.com>
2023-09-18 18:16:21 +0000

    reverse array method

---
+++ Arrays/reverseArray.js
@@ -0,0 +1,13 @@
+function reverseArray(arr) {
+  let low = 0,
+    high = arr.length - 1;
+
+  while (low < high) {
+    let temp = arr[low];
+    arr[low] = arr[high];
+    arr[high] = temp;
+
+    low++;
+    high--;
+  }
+}
