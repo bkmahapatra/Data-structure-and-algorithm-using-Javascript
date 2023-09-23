cc93f3598fef50706237160c44f8eaf32a10130a
BK Mahapatra <39907792+bkmahapatra@users.noreply.github.com>
2023-09-23 18:16:06 +0000

    Create isArmstrong.js

---
+++ Basic/isArmstrong.js
@@ -0,0 +1,22 @@
+/* A positive integer is called an Armstrong number (of order n) if
+abcd... = an + bn + cn + dn + ... */
+// armstrong numbers of n digit
+function isArmstrong(num) {
+  let temp = num,
+    sum = 0,
+    numOfDigit = num.toString().length;
+  console.log(numOfDigit);
+
+  while (temp > 0) {
+    let remainder = temp % 10;
+    sum += remainder ** numOfDigit;
+    temp = parseInt(temp / 10);
+  }
+
+  if (num == sum) {
+    return `${num} is an Armstrong number`;
+  } else {
+    return `${num} is not an Armstrong number`;
+  }
+}
+console.log(isArmstrong(153));
